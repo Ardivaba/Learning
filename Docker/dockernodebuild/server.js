4cf3a4b8ef5ad4c8ca2847a2cc306f8ca3746e90
Ardi Vaba <ardivaba@gmail.com>
2017-03-07 12:56:53 +0200

    Making a docker mess...need to take another hour to understand Docker

---
+++ Docker/dockernodebuild/server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var PORT = 8080
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello from docker container...')
+})
+
+app.listen(PORT)
+console.log('Running on http://localhost' + PORT)
