f9cc2245b017bce21f79b631d8e113a326fea3ee
Ardi Vaba <ardivaba@gmail.com>
2017-03-09 12:31:21 +0200

    thread.join

--- Ruby/Threading/app.rb
+++ Ruby/Threading/app.rb
@@ -1,9 +1,12 @@
 require 'thread'
 
+lock = Mutex.new
 threads = (1..100).map do |number|
   Thread.new do
     sleep 0.5
+    lock.lock
     print "\nThread finished: #{number}"
+    lock.unlock
   end
 end
 
